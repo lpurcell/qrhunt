3b0e37909e4c0d2e3ca6d931ae2cf11b1c853f39
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-08-17 18:32:02 -0500

    Created Extra points page, added links, added css

---
+++ application/views/pages/extra_points.php
@@ -0,0 +1,9 @@
+<h1>Get EXTRA Points</h1>
+<p>Earn extra points for your group by finding these locations and scanning the QR Code with your phone.</p><br/>
+<div class="wrap_list">
+    <ol>
+        <li>Clock Tower</li>
+        <li>Financial Aid</li>
+        <li>Baker Fitness Center</li>
+    </ol>
+</div>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,7 +5,7 @@
             echo "<li>" . anchor('scan/create', 'Create a Scan Manually', 'title="Create a Scan Manually"'). "</li>";
             echo "<li>" . anchor('scan/totals', 'View Points by All Participants', 'title="View Scan Totals"'). "</li>";
             echo "<li>" . anchor('scan/group_totals', 'View Points by Group', 'title="View Group Totals"'). "</li>";
-            echo "<li>" . anchor('', 'Get EXTRA Points!', 'title="Get EXTRA Points"'). "</li>";
+            echo "<li>" . anchor('extra_points', 'Get EXTRA Points!', 'title="Get EXTRA Points"'). "</li>";
         ?>
     </ul>
 </div>
