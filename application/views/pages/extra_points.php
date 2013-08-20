2c2fd0a6fe4a68dde180b31ed0e15b34865525a7
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-08-20 16:36:49 -0500

    Rough Draft of ScaHunt and Made link to it on Profile

--- application/views/pages/extra_points.php
+++ application/views/pages/extra_points.php
@@ -1,9 +1,22 @@
 <h1>Get EXTRA Points</h1>
-<p>Earn extra points for your group by finding these locations and scanning the QR Code with your phone.</p><br/>
+<h3>Earn extra points for your group by finding these locations and scanning the QR Code with your phone.</h3><br/>
 <div class="wrap_list">
     <ol>
         <li>Clock Tower</li>
         <li>Center of Academic Support</li>
+        <li>Student Success</li>
+        <li>Financial Aid</li>
+        <li>The Esry</li>
+        <li>Murphy Computer Lab</li>
+        <li>Agenstein 129</li>
+        <li>Swimming Pool</li>
         <li>Baker Fitness Center</li>
+        <li>Registrar's Office</li>
+        <li>Art Gallery</li>
+        <li>Craig School of Business Office</li>
+        <li>Department of Nursing</li>
+        <li>Help Desk at the Library?</li>
+        <li>IT - 110 at library?</li>
+        <li>The Atrium?</li>
     </ol>
 </div>
--- application/views/register/view.php
+++ application/views/register/view.php
@@ -9,6 +9,7 @@
                     <li><a href="<?php echo site_url('scanned_by/'.$participant_item->QRCode)?>">See Who Scanned You</a></li>
                     <li><a href="<?php echo site_url('scan/totals')?>">View Points by All Participants</a></li>
                     <li><a href="<?php echo site_url('scan/group_totals')?>">View Points by Group</a></li>
+                    <li><a href="<?php echo site_url('extra_points')?>">Get EXTRA Points!</a></li>
                 </ul>
             </div>
             <br/>
