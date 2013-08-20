15d01788e09b29a03c2b382dbcccc48760986980
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-08-20 11:59:53 -0500

    Changed Confirmation Message

--- application/views/news/success.php
+++ application/views/news/success.php
@@ -1,5 +1,5 @@
-<h1>CONGRATULATIONS!!</h1>
-
-<p>Yay! You did it!</p>
+<h1>Success!!</h1>
+<h2>Scan your QR Code again to view your Profile.</h2>
+<h3>You will now be redirected to the home page.</h3>
 <?php
 $this->output->set_header('refresh:3;url ='.site_url()); ?>
