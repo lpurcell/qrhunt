f4b2dcb5b13404f8a15b055e16fe2b318105fc80
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-08-20 20:24:41 -0500

    Deleted Create Scan from Home Page

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,8 +2,7 @@
 <div class="menu">
     <ul>
         <?php
-            echo "<li>" . anchor('scan/create', 'Create a Scan Manually', 'title="Create a Scan Manually"'). "</li>";
-           /* echo "<li>" . anchor('scan/totals', 'View Points by All Participants', 'title="View Scan Totals"'). "</li>";
+            /* echo "<li>" . anchor('scan/totals', 'View Points by All Participants', 'title="View Scan Totals"'). "</li>";
             echo "<li>" . anchor('scan/group_totals', 'View Points by Group', 'title="View Group Totals"'). "</li>"; */
             echo "<li>" . anchor('extra_points', 'Get EXTRA Points!', 'title="Get EXTRA Points"'). "</li>";
         ?>
